77f88280e852df7e95aa1187eb5ac8d26893cad3
Volodymyr_Kartaiev <kartayev@gmail.com>
2020-09-10 19:04:30 +0300

    header shadow

--- client/components/layout/Footer.js
+++ client/components/layout/Footer.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return (
-    <div className="w-full h-12 mt-auto border-t border-gray-200 flex justify-center bg-white items-center">
-      Footer
-    </div>
-  );
+  return <div className="footer">Footer</div>;
 };
 
 export default Footer;
--- client/components/layout/Header.js
+++ client/components/layout/Header.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Nav from "./Nav";
+import { GlobalContext } from "../../context/global.context";
 
 const Header = () => {
+  const { isOpened } = useContext(GlobalContext);
+
+  const [pos, setPos] = useState("top");
+
+  useEffect(() => {
+    document.addEventListener("scroll", (e) => {
+      let scrolled = document.scrollingElement.scrollTop;
+      if (scrolled >= 5) {
+        setPos("moved");
+      } else {
+        setPos("top");
+      }
+    });
+  }, []);
+
+  const shadow = isOpened || pos === "moved" ? "shadow-md" : "";
   return (
-    <div className="header">
+    <div className={`header ${shadow}`}>
       <div className="w-11/12 flex justify-between items-center">
         <div className="text-gray-900 font-bold text-2xl xl:text-5xl font-logo">
           Арминианская перспектива
