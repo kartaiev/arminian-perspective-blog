e5864c76820dbd1977bd8f423aa8fecad647ab85
Volodymyr_Kartaiev <kartayev@gmail.com>
2020-09-25 08:22:26 +0300

    footer problem on ipad

--- client/components/layout/Footer.js
+++ client/components/layout/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-full h-10 px-4 border-t border-gray-300 flex-shrink-0 flex items-center justify-center text-gray-600">
+    <footer className="footer w-full h-10 px-4 border-t border-gray-300 flex-shrink-0 flex items-center justify-center text-gray-600">
       <div className="text-sm">&copy; copyright 2020</div>
     </footer>
   );
--- client/components/layout/Layout.js
+++ client/components/layout/Layout.js
@@ -4,11 +4,11 @@ import Footer from "components/layout/Footer";
 
 const Layout = ({ children }) => {
   return (
-    <div className="min-h-screen px-6 flex flex-col font-primary">
+    <>
       <Header />
       <main className="px-0 flex flex-col flex-1">{children}</main>
       <Footer />
-    </div>
+    </>
   );
 };
 
